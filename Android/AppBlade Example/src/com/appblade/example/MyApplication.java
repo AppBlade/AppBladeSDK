d3498f676a45d7135afeb958b09bceffef4694b7
Dylan James <Dylan.James@raizlabs.com>
2012-05-30 11:08:32 -0400

    Remove SDK testing keys.

--- Android/AppBlade Example/src/com/appblade/example/MyApplication.java
+++ Android/AppBlade Example/src/com/appblade/example/MyApplication.java
@@ -12,10 +12,10 @@ public class MyApplication extends Application {
 	
 		// Populate with tokens from your application settings
 		// see README for details	
-		String uuid = "ca460dcb-b7c2-43c1-ba50-8b6cda63f369";
-		String token = "8f1792db8a39108c14fa8c89663eec98";
-		String secret = "c8536a333fb292ba46fc98719c1cfdf6";
-		String issuance = "1316609918";
+		String uuid = "";
+		String token = "";
+		String secret = "";
+		String issuance = "";
 		
 		AppBlade.register(this, token, secret, uuid, issuance);
 	}
