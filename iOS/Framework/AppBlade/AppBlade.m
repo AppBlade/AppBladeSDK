5a3eb455c4de29256cd68d6c34038684731c9485
James Daniels <james@marginleft.com>
2012-09-21 18:24:42 -0400

    Incremented the SDK patch version

--- iOS/Framework/AppBlade/AppBlade.m
+++ iOS/Framework/AppBlade/AppBlade.m
@@ -16,7 +16,7 @@
 #import "asl.h"
 #import <QuartzCore/QuartzCore.h>
 
-static NSString* const s_sdkVersion                     = @"0.2";
+static NSString* const s_sdkVersion                     = @"0.2.1";
 
 const int kUpdateAlertTag                               = 316;
 
