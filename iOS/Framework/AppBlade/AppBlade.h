4ea1b5dcb31654fd12b59a8e3f96fa24692bb4cc
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2012-11-08 12:29:24 -0500

    added some friendly helper text to the comments

--- iOS/Framework/AppBlade/AppBlade.h
+++ iOS/Framework/AppBlade/AppBlade.h
@@ -42,7 +42,7 @@ static NSString* const kAppBladeCacheDirectory;
     NSString *_appBladeProjectIssuedTimestamp;
 }
 
-// AppBlade host name
+// AppBlade host name //Include neither http:// nor https://, we'll handle that.
 @property (nonatomic, retain) NSString* appBladeHost;
 
 // UUID of the project on AppBlade.
