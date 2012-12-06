5f212d2b0416782ba34c6bc9634c6394caf6a865
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2012-12-06 18:47:29 -0500

    PostSessions pluralized to use List<SessionData>. Requests use JSON formatting

--- Android/AppBlade Framework/src/com/appblade/framework/stats/SessionHelper.java
+++ Android/AppBlade Framework/src/com/appblade/framework/stats/SessionHelper.java
@@ -21,6 +21,8 @@ import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.ContentBody;
 import org.apache.http.entity.mime.content.StringBody;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.appblade.framework.AppBlade;
 import com.appblade.framework.WebServiceHelper;
@@ -28,6 +30,8 @@ import com.appblade.framework.WebServiceHelper.HttpMethod;
 import com.appblade.framework.utils.HttpClientProvider;
 import com.appblade.framework.utils.IOUtils;
 
+import android.content.Context;
+import android.os.Environment;
 import android.util.Log;
 
 public class SessionHelper {
@@ -37,15 +41,20 @@ public class SessionHelper {
 	
 	//API related functions
 	public static Boolean postSession(SessionData data) {
+		ArrayList<SessionData> sessionsList = new ArrayList<SessionData>();
+		sessionsList.add(data);
+		return postSessions(sessionsList);
+	}
+
+	private static boolean postSessions(List<SessionData> sessionsList) {
 		boolean success = false;
 		HttpClient client = HttpClientProvider.newInstance("Android");
-
 		try
 		{
 			String urlPath = String.format(WebServiceHelper.ServicePathSessionFormat, AppBlade.appInfo.AppId, AppBlade.appInfo.Ext);
 			String url = WebServiceHelper.getUrl(urlPath);
 
-			final MultipartEntity content = SessionHelper.getPostSessionBody(data, AppBlade.BOUNDARY);
+			final MultipartEntity content = SessionHelper.getPostSessionBody(sessionsList, AppBlade.BOUNDARY);
 
 			HttpPut request = new HttpPut();
 			request.setEntity(content);
@@ -73,6 +82,7 @@ public class SessionHelper {
 			{
 				int statusCode = response.getStatusLine().getStatusCode();
 				int statusCategory = statusCode / 100;
+				Log.d(AppBlade.LogTag, "response: "+ statusCode);
 
 				if(statusCategory == 2)
 					success = true;
@@ -88,25 +98,6 @@ public class SessionHelper {
 		return success;
 	}
 	
-	public static MultipartEntity getPostSessionBody(SessionData data, String boundary) {
-		MultipartEntity entity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE, boundary, null);
-		
-		try
-		{
-			ContentBody deviceIdBody  = new StringBody(AppBlade.appInfo.AppId);
-			entity.addPart("session[device_id]", deviceIdBody);			
-			ContentBody projectIdBody  = new StringBody(AppBlade.appInfo.AppId);
-			entity.addPart("session[project_id]", projectIdBody);			
-			ContentBody sessionsBody = new StringBody(data.formattedSessionBody());
-			entity.addPart("session[sessions]", sessionsBody);			
-		} 
-		catch (IOException e) {
-			Log.d(AppBlade.LogTag, e.toString());
-		}
-		
-		return entity;
-	}
-
 	public static MultipartEntity getPostSessionBody(List<SessionData> sessions, String boundary) {
 		MultipartEntity entity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE, boundary, null);
 		
@@ -126,72 +117,90 @@ public class SessionHelper {
 		return entity;
 	}
 	
-	private static void postExistingSessions(){
-		File f = new File(sessionsIndexFileLocation());
+	public static void postExistingSessions(Context context){
+		Log.d(AppBlade.LogTag, "checking for existing sessions.");
+
+		File f = new File(sessionsIndexFileURI(context));
 		if(f.exists()){
-			ArrayList<SessionData> sessionsList = new ArrayList<SessionData>();
+			Log.d(AppBlade.LogTag, sessionsIndexFileURI(context)+" exists.");
+
+			List<SessionData> sessionsList = readData(context);
 			if(sessionsList.size() != 0){
 				Log.d(AppBlade.LogTag, "Finished sessions exist, posting them.");
-				SessionData s = sessionsList.get(0);
-				if(postSession(s)){
-					Log.d(AppBlade.LogTag, "session posting success. remove successful session.");
-					removeSession(s);
+				if(postSessions(sessionsList)){
+					Log.d(AppBlade.LogTag, "session posting success. remove successful sessions");
+					SessionData lastSession = sessionsList.get(sessionsList.size()-1);
+					removeSessionsEndedBefore(context, lastSession.ended);
 				}else{
-					Log.d(AppBlade.LogTag, "error posting session. Stored for later.");
-					
+					Log.d(AppBlade.LogTag, "error posting session. Stored for later.");					
 				}
 			}
+		}else{
+			Log.d(AppBlade.LogTag, "Sessions file does not exist, creating it.");
+			List<SessionData> blankSessions = new ArrayList<SessionData>();
+			updateFile(context, sessionsIndexFileURI(context), blankSessions);
 		}
 	}
 	
-	public static void startSession(){
-		//check for existing sessions, post them.
-		postExistingSessions();
+	public static void startSession(Context context){
 		Log.d(AppBlade.LogTag, "Starting Session");
 		AppBlade.currentSession = new SessionData();
 	}
 	 
 
-	public static void endSession(){
+	public static void endSession(Context context){
 		Log.d(AppBlade.LogTag, "Ending Session");
 		if(AppBlade.currentSession != null){
 			AppBlade.currentSession.ended = new Date();
 			SessionData sessionToStore = new SessionData(AppBlade.currentSession.began, AppBlade.currentSession.ended);
-			insertSessionData(sessionToStore);
+			insertSessionData(context, sessionToStore);
 			AppBlade.currentSession = null;
 		}
 	}
 
-	//sessions formatting
+	//sessions batch formatting
 	public static String formattedSessionsBodyFromList(List<SessionData> sessions){
-		String toRet = "\"sessions\" : ["; 
-		for(int i = 0; i < sessions.size(); i++){
-			SessionData s = sessions.get(i);
-			toRet = toRet + " " + s.formattedSessionBody();
-			if(sessions.size() > 1 && i < sessions.size() - 1){
-				toRet = toRet +",";	
-			}
+		String toRet = "none";
+
+		JSONObject jsonRet = new JSONObject();
+		ArrayList<JSONObject> jsonSessions = new ArrayList<JSONObject>();
+		
+		for(SessionData s : sessions){
+			jsonSessions.add(s.formattedSessionAsJSON());
 		}
-		toRet = toRet  + "]";
+		
+		try {
+			jsonRet.put("sessions", jsonSessions);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		
+		toRet = jsonRet.toString();
+		
 		return toRet;
 	}
 	
 	//Sessions storage/queue logic
-	public static String sessionsIndexFileLocation() {
-		return sessionsFolder + "/"+sessionsIndexFileName;
+	public static String sessionsIndexFileURL(Context context) {
+		File f = context.getFilesDir();
+		return f.getAbsolutePath() + sessionsFolder;
+	}
+	
+	public static String sessionsIndexFileURI(Context context) {
+		return sessionsIndexFileURL(context) + "/"+sessionsIndexFileName;
 	}
 
 	
-	public static SessionData createPersistentSession() {
+	public static SessionData createPersistentSession(Context context) {
 		Log.d(AppBlade.LogTag, "Creating New Session ");
 		SessionData data = new SessionData(new Date(), new Date());
-		insertPendingSession(data);
+		insertPendingSession(context, data);
 		return data;
 	}
 	
-	public static void insertPendingSession(SessionData sessionToInsert){
+	public static void insertPendingSession(Context context, SessionData sessionToInsert){
 		//check if file exists
-		File f = new File(sessionsIndexFileLocation());
+		File f = new File(sessionsIndexFileURI(context));
 		if(f.exists()){
 			try {
 				f.createNewFile();
@@ -200,12 +209,12 @@ public class SessionHelper {
 			}
 		}
 		
-		insertSessionData(sessionToInsert);
+		insertSessionData(context, sessionToInsert);
 	}
 
 
-	public synchronized static void removeSessionsEndedBefore(Date dateEnded){
-        List<SessionData> sessionDataList = readData();
+	public synchronized static void removeSessionsEndedBefore(Context context, Date dateEnded){
+        List<SessionData> sessionDataList = readData(context);
         // remove all objects from ArrayList that ended before dateEnded
         for (int i = 0; i < sessionDataList.size(); )
         {
@@ -220,45 +229,44 @@ public class SessionHelper {
         	}
         }
         //list filtered, rewrite
-        updateFile(sessionsIndexFileLocation(), sessionDataList);
+        updateFile(context, sessionsIndexFileURI(context), sessionDataList);
 	}
 	
-	public synchronized static void removeSession(SessionData data) {
+	public synchronized static void removeSession(Context context, SessionData data) {
 		Log.d(AppBlade.LogTag, "Removing Session to file");
-        List<SessionData> sessionDataList = readData();
+        List<SessionData> sessionDataList = readData(context);
         // remove object from ArrayList
         sessionDataList.remove(data);
         // Update file
-        updateFile(sessionsIndexFileLocation(), sessionDataList);
+        updateFile(context, sessionsIndexFileURI(context), sessionDataList);
 	}
 	
 
-	public synchronized static void insertSessionData(SessionData data) {
+	public synchronized static void insertSessionData(Context context, SessionData data) {
 		Log.d(AppBlade.LogTag, "Adding Session to file");
 
-        List<SessionData> userDataList = readData();
+        List<SessionData> userDataList = readData(context);
         userDataList.add(data);
         // Update file
-        updateFile(sessionsIndexFileLocation(), userDataList);
+        updateFile(context, sessionsIndexFileURI(context), userDataList);
 	}	
 	
 
-	public synchronized  static List<SessionData> readData()
+	public synchronized  static List<SessionData> readData(Context context)
     {
+    	Log.d(AppBlade.LogTag, "reading in Sessions file. ");
+
             List<SessionData> listofusers = new ArrayList<SessionData>();
             FileInputStream fstream = null;
             try
             {
 
-                fstream = new FileInputStream(sessionsIndexFileLocation());
+                fstream = new FileInputStream(sessionsIndexFileURI(context));
                 BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
                 String strLine = "";
-                String[] tokens = strLine.split(", ");
                 //Read file line by line
                 while ((strLine = br.readLine()) != null)   {
-                  // Copy the content into the array
-                  tokens = strLine.split(", ");
-                  listofusers.add(new SessionData(new Date(tokens[0]), new Date(tokens[1])));
+                  listofusers.add(new SessionData(strLine));
                 }
             }
             catch (IOException e) {
@@ -267,14 +275,42 @@ public class SessionHelper {
             finally {
                 try { fstream.close(); } catch ( Exception ignore ) {}
             }
+            
+        	Log.d(AppBlade.LogTag, "Read Sessions file. " +listofusers.size() +" sessions");
+
             return listofusers;
     }
 
 
-    public synchronized  static void updateFile(String filename, List<SessionData> userDataList) {
+    public synchronized  static void updateFile(Context context, String filename, List<SessionData> userDataList) {
+    	//check for existence of file, if no file, create file
+    	Log.d(AppBlade.LogTag, "Updating Sessions file. " +userDataList.size() +" sessions");
+
+    try{
+    	final File parent = new File(sessionsIndexFileURL(context));
+    	if(!parent.exists())
+    	{
+    		System.err.println("Parent directories do not exist");
+	    	if (!parent.mkdirs())
+	    	{
+	    	   System.err.println("Could not create parent directories");
+	    	}
+    	}
+    	final File someFile = new File(sessionsIndexFileURL(context), sessionsIndexFileName);
+    	if(!someFile.exists()){
+        	Log.d(AppBlade.LogTag, "Sessions file does not exist yet. creating Sessions file.");
+    		someFile.createNewFile();
+    	}
+    }catch (IOException ex) {
+    	Log.d(AppBlade.LogTag, "Error making Sessions file");
+    	ex.printStackTrace();
+    }
+    	
        BufferedWriter bufferedWriter = null;
         try {
             bufferedWriter = new BufferedWriter(new FileWriter(filename));
+        	Log.d(AppBlade.LogTag, "built bufferedWriter");
+
         } catch (IOException ex) {
         	Log.d(AppBlade.LogTag, "Error writing Sessions file");
         	ex.printStackTrace();
@@ -283,8 +319,9 @@ public class SessionHelper {
         String row;
        for(int i=0; i<userDataList.size(); i++) {
            ud = userDataList.get(i);
-           row = ud.began.toString() + ", " + ud.ended.toString();
+           row = ud.sessionAsStoredString();
             try {
+            	Log.d(AppBlade.LogTag, "writing "+row);
                 bufferedWriter.write(row);
                 bufferedWriter.newLine();
             } catch (FileNotFoundException ex) {
@@ -296,6 +333,8 @@ public class SessionHelper {
         //Close the BufferedWriter
         try {
               if (bufferedWriter != null) {
+              	Log.d(AppBlade.LogTag, "teardown bufferedWriter");
+
                   bufferedWriter.flush();
                   bufferedWriter.close();
               }
