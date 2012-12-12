a5ed2e499a261282777a5ac89549ee4e73956497
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2012-12-11 20:10:17 -0500

    Boilerplate store/load stuff for JSON wrapper

--- Android/AppBlade Framework/src/com/appblade/framework/customparams/CustomParamData.java
+++ Android/AppBlade Framework/src/com/appblade/framework/customparams/CustomParamData.java
@@ -2,6 +2,7 @@ package com.appblade.framework.customparams;
 
 import java.util.Iterator;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.Context;
@@ -13,17 +14,30 @@ public class CustomParamData extends JSONObject {
 	}
 	
 	//helper functions for parameter data
-	public void storeCurrentData()
+	public void storeCurrentData(Context context)
 	{
-		
+		CustomParamDataHelper.storeCurrentCustomParams(context, this);
 	}
 	
+	@SuppressWarnings("unchecked")
 	public CustomParamData refreshFromStoredData(Context context)
 	{
-		//clobber the keys
-		Iterator keysToRemove = this.keys();
-		
-		
-		return CustomParamDataHelper.getCurrentCustomParams(context);
+		CustomParamData latestParams = CustomParamDataHelper.getCurrentCustomParams(context);
+		//clobber the keys, replace with latest params
+		Iterator<String> keysToRemove = this.keys();
+		Iterator<String> keysToAdd = latestParams.keys(); 
+		while(keysToRemove.hasNext()){
+			String nextKey = (String) keysToRemove.next();
+			this.remove(nextKey);
+		}
+		while(keysToAdd.hasNext()){
+			String nextKey = (String) keysToAdd.next();
+			try {
+				this.put(nextKey, latestParams.get(nextKey));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		return this; //return new self
 	}
 }
--- Android/AppBlade Framework/src/com/appblade/framework/customparams/CustomParamDataHelper.java
+++ Android/AppBlade Framework/src/com/appblade/framework/customparams/CustomParamDataHelper.java
@@ -1,34 +1,46 @@
 package com.appblade.framework.customparams;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.appblade.framework.AppBlade;
 import com.appblade.framework.utils.StringUtils;
 
 import android.content.Context;
+import android.util.Log;
 
 public class CustomParamDataHelper {
 	//I/O RELATED
+	//Just store the json straight to file
 	public static String customParamsFolder = "/appBlade"; 
-	public static String customParamsFileName = "customParams.txt";
+	public static String customParamsFileName = "customParams.json";
 	//API RELATED
 	public static String customParamsIndexMIMEType = "text/json"; 
 
-	
-	//Just store the json straight to file
 	public static String jsonFileURL(Context context)
 	{
-		return customParamsFileName;
+		return customParamsFolder;
+	}
+	public static String jsonFileURI(Context context)
+	{
+		return jsonFileURL(context)+"/"+customParamsFileName;
 	}
 	
 	
 	public static CustomParamData getCurrentCustomParams(Context context)
 	{
-		//don't need to do much currently since all it is is a JSON object
+		//don't need to do much currently since all it is is a JSON object, 
+		//other features may include behaviors we'll need in separate behavior, ttl for example
 		return (CustomParamData)getCustomParamsAsJSON(context);
 	}
+
 	
 	// JSON Parsing
 	public static JSONObject getCustomParamsAsJSON(Context context){
@@ -44,4 +56,43 @@ public class CustomParamDataHelper {
         return json;
 	}
 	
+	
+	public static void storeCurrentCustomParams(Context context, CustomParamData customParams)
+	{
+		String stringJSON = ((JSONObject) customParams).toString();
+		//confirm file existence
+	    try{
+	    	final File parent = new File(jsonFileURL(context));
+	    	if(!parent.exists())
+	    	{
+	    		System.err.println("Parent directories do not exist");
+		    	if (!parent.mkdirs())
+		    	{
+		    	   System.err.println("Could not create parent directories");
+		    	}
+	    	}
+	    	final File someFile = new File(jsonFileURL(context), customParamsFileName);
+	    	if(!someFile.exists()){
+	        	Log.d(AppBlade.LogTag, "customParams file does not exist yet. creating Sessions file.");
+	    		someFile.createNewFile();
+	    	}
+	    }catch (IOException ex) {
+	    	Log.d(AppBlade.LogTag, "Error making customParams file");
+	    	ex.printStackTrace();
+	    }
+	    
+        try {
+            //open the buffered writer
+        	BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(customParamsFileName));
+    		Log.d(AppBlade.LogTag, "writing customParams "+stringJSON);
+            bufferedWriter.write(stringJSON);
+	        //Close the BufferedWriter
+			bufferedWriter.flush();
+			bufferedWriter.close();
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+	}
 }
