50c077970c9ffb3024b0bd1b1b2927018ae8d513
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2012-12-18 14:38:53 -0500

    Updating MainActivity with some example Custom Parameters

--- Android/AppBlade Example/src/com/appblade/example/MainActivity.java
+++ Android/AppBlade Example/src/com/appblade/example/MainActivity.java
@@ -16,92 +16,89 @@ import com.appblade.framework.authenticate.RemoteAuthHelper;
 public class MainActivity extends Activity {
 
 	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        initControls();
-    }
-    
-    public void onResume() {
-    	super.onResume();
-    	AppBlade.startSession(this.getApplicationContext());
-    	AppBlade.authorize(this);
-    	
-    	AppBlade.setCustomParameter(getApplicationContext(), "AppState", "Resumed");
-    }
-    
-    public void onPause() {
-    	super.onPause();
-    	AppBlade.endSession(getApplicationContext()); 
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
 
+		initControls();
+	}
+
+	public void onResume() {
+		super.onResume();
+		AppBlade.startSession(this.getApplicationContext());
+		AppBlade.authorize(this);
+
+		AppBlade.setCustomParameter(getApplicationContext(), "AppState",
+				"Resumed");
+	}
+
+	public void onPause() {
+		super.onPause();
+		AppBlade.endSession(getApplicationContext());
+	}
 
 	private void initControls() {
 		View btnDivideByZero = findViewById(R.id.btnDivideByZero);
 		View btnClearAuthData = findViewById(R.id.btnClearAuthData);
 		View btnFeedback = findViewById(R.id.btnFeedback);
-		
-		btnDivideByZero.setOnClickListener(new OnClickListener() {			
+
+		btnDivideByZero.setOnClickListener(new OnClickListener() {
 			@SuppressWarnings("unused")
 			public void onClick(View v) {
-				try
-				{
+				try {
 					int divideByZero = 1 / 0;
-				}
-				catch(ArithmeticException ex)
-				{
+				} catch (ArithmeticException ex) {
 					AppBlade.notify(ex);
-				}
-				catch(Exception ex)
-				{
+				} catch (Exception ex) {
 					// General exceptions not reported
 				}
 			}
 		});
-		
+
 		btnClearAuthData.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				RemoteAuthHelper.clear(MainActivity.this);
 				KillSwitch.clear(MainActivity.this);
 				AlertDialog.Builder builder = new Builder(MainActivity.this);
 				builder.setMessage("Auth data cleared");
-				builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int which) {
-				    	AppBlade.authorize(MainActivity.this, false);
-					}
-				});
+				builder.setPositiveButton("OK",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int which) {
+								AppBlade.authorize(MainActivity.this, false);
+							}
+						});
 				builder.show();
 			}
 		});
-		
+
 		btnFeedback.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				MainActivity.this.doFeedbackWithScreenshot();
 			}
 		});
 	}
-	
+
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		int action = event.getAction();
-		switch(action & MotionEvent.ACTION_MASK)
-		{
+		switch (action & MotionEvent.ACTION_MASK) {
 		case MotionEvent.ACTION_POINTER_DOWN:
 			if (event.getPointerCount() == 3) {
 				doFeedbackWithScreenshot();
 				return true;
 			}
 		}
-		
+
 		return super.onTouchEvent(event);
 	}
-	
+
 	private void doFeedbackWithScreenshot() {
-    	AppBlade.setCustomParameter(getApplicationContext(), "AppState", "Did A Feedback");
+		AppBlade.setCustomParameter(getApplicationContext(), "AppState",
+				"Did A Feedback");
 
 		AppBlade.doFeedbackWithScreenshot(this, this);
 	}
-	
+
 }
