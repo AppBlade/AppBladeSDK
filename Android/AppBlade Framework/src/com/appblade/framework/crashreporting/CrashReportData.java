3438844bd05c88fb4f4d000b686fb57d6a19b572
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-01-02 18:15:58 -0500

    adding a TODO

--- Android/AppBlade Framework/src/com/appblade/framework/crashreporting/CrashReportData.java
+++ Android/AppBlade Framework/src/com/appblade/framework/crashreporting/CrashReportData.java
@@ -2,6 +2,7 @@ package com.appblade.framework.crashreporting;
 
 public class CrashReportData {
 	public Throwable exception;
+	//TODO: save a copy of the custom_params json file at moment of crash, report that instead of the file that the user's always changing. 
 	
 	public CrashReportData(Throwable e) {
 		exception = e;
