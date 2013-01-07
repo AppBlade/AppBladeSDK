61f10cfc382881ca83cf3b4f97a7658a66f67a0f
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-01-07 15:31:19 -0500

    Docs for RemoteAuthorizeActivity and internal JavascriptInterface class

--- Android/AppBlade Framework/src/com/appblade/framework/authenticate/RemoteAuthorizeActivity.java
+++ Android/AppBlade Framework/src/com/appblade/framework/authenticate/RemoteAuthorizeActivity.java
@@ -1,5 +1,6 @@
 package com.appblade.framework.authenticate;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.graphics.Bitmap;
@@ -14,6 +15,12 @@ import com.appblade.framework.AppBlade;
 import com.appblade.framework.WebServiceHelper;
 import com.appblade.framework.authenticate.AuthTokensDownloadTask.OnPostExecuteListener;
 
+@SuppressLint("SetJavaScriptEnabled")
+/**
+ * Activity to prompt the user to authorize themselves to use the app. 
+ * Prompts a WebView that talks to AppBlade where the user signs in with their valid credentials.
+ * WARNING: Uses javascript. If you are using a custom endpoint make sure you trust the site you are accessing.
+ */
 public class RemoteAuthorizeActivity extends Activity {
 	
 	private static final String EndpointAuthNew = "/oauth/authorization/new?client_id=%s&response_type=code";
@@ -33,6 +40,9 @@ public class RemoteAuthorizeActivity extends Activity {
         initControls();
     }
 
+    /**
+     * Initializes the WebView and defines the WebClient behavior.
+     */
 	private void initControls() {
         String path = String.format(EndpointAuthNew, AppBlade.appInfo.Token);
         final String authUrl = WebServiceHelper.getUrl(path);
@@ -51,47 +61,39 @@ public class RemoteAuthorizeActivity extends Activity {
         });
 
         webview.setWebViewClient(new WebViewClient() {
-
 			@Override
 			public void onLoadResource(WebView view, String url) {
-//				Log.d(AppBlade.LogTag, String.format("onLoadResource url: %s", url));
 				super.onLoadResource(view, url);
 			}
-
 			@Override
 			public void onPageFinished(WebView view, String url) {
-				Log.d(AppBlade.LogTag, String.format("onPageFinished url: %s", url));
 				super.onPageFinished(view, url);
-				
 				if(progress != null && progress.isShowing())
 					progress.dismiss();
 			}
-
 			@Override
 			public void onPageStarted(WebView view, String url, Bitmap favicon) {
-				Log.d(AppBlade.LogTag, String.format("onPageStarted url: %s", url));
 				super.onPageStarted(view, url, favicon);
-				
 				if(progress == null || !progress.isShowing())
 					progress = ProgressDialog.show(RemoteAuthorizeActivity.this, null, "loading...");
 			}
-        	
         });
         
 		webview.loadUrl(authUrl);
 	}
 	
+	/**
+	 * Javascript interface that will download the AuthTokens to local storage (inside RemoteAuthHelper) on notification. It confirms the storage by reauthorizing through AppBlade.
+	 */
 	class JavascriptInterface {
 		public void notifyAuthCode(final String code) {
 			final String message = String.format("JavascriptInterface.notifyAuthCode code: %s", code);
 			Log.d(AppBlade.LogTag, message);
 			
 			runOnUiThread(new Runnable() {
-				
 				public void run() {
 					AuthTokensDownloadTask task = new AuthTokensDownloadTask(RemoteAuthorizeActivity.this);
 					task.setOnPostExecuteListener(new OnPostExecuteListener() {
-						
 						public void onPostExecute() {
 							AppBlade.authorize(RemoteAuthorizeActivity.this, true);
 						}
