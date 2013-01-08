e3a971ace3d5e566f89028de04df709188c1317d
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-01-08 10:51:52 -0500

    Documenting StringUtils

--- Android/AppBlade Framework/src/com/appblade/framework/utils/StringUtils.java
+++ Android/AppBlade Framework/src/com/appblade/framework/utils/StringUtils.java
@@ -13,11 +13,13 @@ import java.security.NoSuchAlgorithmException;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
-import android.util.Log;
 
-import com.appblade.framework.AppBlade;
-
-
+/**
+ * Utility class for helpful String methods
+ * Contains all MD5, HMAC, and SHA2 hashing methods at the String level that you should need. See {@link com.appblade.framework.utils.Base64} for the deeper implementation. 
+ * @see javax.crypto.Mac
+ * @see javax.crypto.spec.SecretKeySpec
+ */
 public class StringUtils {
 	 public static final int BUFFER_SIZE = 2048;
 	 
@@ -47,16 +49,50 @@ public class StringUtils {
 		return sb.toString();
 	}
 	
+	/**
+	 * Checks whether the input string is either null or has a trimmed length of zero.
+	 * @param input String to check 
+	 * @return true on null or if there are zero non-whitespace characters in the string.
+	 */
 	public static boolean isNullOrEmpty(String input)
 	{
 		return input == null || input.trim().length() == 0;
 	}
 	
+	/**
+	 * Appends a formatted string (with objects) to an existing StringBuilder. StringBuilder will contain the appended string and objects on success.
+	 * @param builder The reference to the StringBuilder to alter.
+	 * @param format String to append, can contain formatting.
+	 * @param params Optional objects to include with the format String. Can be null.
+	 */
 	public static void append(StringBuilder builder, String format, Object... params)
 	{
 		builder.append(String.format(format, params));
 	}
 	
+	/**
+	 * Tries to parse a String into an integer, fails silently and returns a fallback value.
+	 * @param input String to parse for an integer.
+	 * @param defaultValue value to return in the event the parsing fails.
+	 * @return the parsed string value on success, or the default value on failure.
+	 */
+	public static int safeParse(String input, int defaultValue) {
+		int value = defaultValue;
+		try
+		{
+			value = Integer.parseInt(input);
+		}
+		catch (Exception ex) { }
+		return value;
+	}
+
+	
+	/**
+	 * Hash-based Message Authentication Code that encrypts a message based on a shared secret. Fails silently.
+	 * @param key The shared secret that both the sender and the receiver possess.
+	 * @param message The message to be encrypted.
+	 * @return An HMAC-SHA256 encrypted message or an empty string in the case of either an InvalidKeyException or NoSuchAlgorithmException. 
+	 */
 	public static String hmacSha256(String key, String message)
 	{
 		String result = "";
@@ -74,6 +110,11 @@ public class StringUtils {
 		return result;
 	}
 	
+	/**
+	 * Secure Hash Algorithm that encrypts a message with a 256-bit digest. Fails silently.
+	 * @param input String message to encrypt.
+	 * @return A byte array of the message digest, or null in the case of a NoSuchAlgorithmException.
+	 */
 	public static byte[] sha256(String input)
 	{
 		try
@@ -87,66 +128,12 @@ public class StringUtils {
 		
 		return null;
 	}
-
-	public static int safeParse(String input, int defaultValue) {
-		int value = defaultValue;
-		try
-		{
-			value = Integer.parseInt(input);
-		}
-		catch (Exception ex) { }
-		return value;
-	}
 	
-	public static String md5(String input) {
-		String hash = input;
-		MessageDigest m = null;
-
-	    try
-	    {
-            m = MessageDigest.getInstance("MD5");
-    	    m.update(input.getBytes(), 0, input.length());
-    	    hash = new BigInteger(1, m.digest()).toString(16);
-	    }
-	    catch (NoSuchAlgorithmException e) { }
-	    return hash;
-	}
-	
-	
-	public static String md5FromInputStream(InputStream is)  {
-		MessageDigest md = null;
-		byte[] byteArray = null;
-		try {
-			md = MessageDigest.getInstance("MD5");
-		} catch (NoSuchAlgorithmException e1) {
-			e1.printStackTrace();
-		}
-		
-		if(md != null){
-			try {
-				is = new DigestInputStream(is, md);
-				// read stream to EOF as normal...
-			}
-			finally {
-				try {
-					is.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-			byteArray = md.digest();
-		}
-		
-        StringBuffer hexString = new StringBuffer();
-        for (int i = 0; i < byteArray.length; i++) {
-            String hex = Integer.toHexString(0xff & byteArray[i]);
-            if(hex.length() == 1) hexString.append('0');
-            hexString.append(hex);
-        }
-        
-        return hexString.toString();
-	}
-
+	/**
+	 * Secure Hash Algorithm that encrypts a message with a 256-bit digest. Fails silently.
+	 * @param is InputStream of the message to encrypt.
+	 * @return A String of the message digest in hexadecimal format, or null in the case of a NoSuchAlgorithmException.
+	 */
 	public static String sha256FromInputStream(InputStream is)  {
 		MessageDigest md = null;
         StringBuffer hexString = new StringBuffer();
@@ -184,4 +171,65 @@ public class StringUtils {
 
         return hexString.toString();
 	}
+
+	
+	/**
+	 * Mostly deprecated due to vulnerabilities with MD5
+	 * @param input
+	 * @return MD5 String
+	 */
+	public static String md5(String input) {
+		String hash = input;
+		MessageDigest m = null;
+
+	    try
+	    {
+            m = MessageDigest.getInstance("MD5");
+    	    m.update(input.getBytes(), 0, input.length());
+    	    hash = new BigInteger(1, m.digest()).toString(16);
+	    }
+	    catch (NoSuchAlgorithmException e) { }
+	    return hash;
+	}
+	
+	
+	/**
+	 * @warning Mostly deprecated due to vulnerabilities with MD5
+	 * @param is inputStream to MD5 
+	 * @return MD5 String
+	 */
+	public static String md5FromInputStream(InputStream is)  {
+		MessageDigest md = null;
+		byte[] byteArray = null;
+		try {
+			md = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e1) {
+			e1.printStackTrace();
+		}
+		
+		if(md != null){
+			try {
+				is = new DigestInputStream(is, md);
+				// read stream to EOF as normal...
+			}
+			finally {
+				try {
+					is.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			byteArray = md.digest();
+		}
+		
+        StringBuffer hexString = new StringBuffer();
+        for (int i = 0; i < byteArray.length; i++) {
+            String hex = Integer.toHexString(0xff & byteArray[i]);
+            if(hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+        
+        return hexString.toString();
+	}
+
 }
