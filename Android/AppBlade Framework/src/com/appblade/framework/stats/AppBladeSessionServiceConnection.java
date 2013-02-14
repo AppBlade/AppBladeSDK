f8ad13bf0a4565942aa935f7782f1da0248242eb
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-02-14 14:57:34 -0500

    Logs

--- Android/AppBlade Framework/src/com/appblade/framework/stats/AppBladeSessionServiceConnection.java
+++ Android/AppBlade Framework/src/com/appblade/framework/stats/AppBladeSessionServiceConnection.java
@@ -1,8 +1,11 @@
 package com.appblade.framework.stats;
 
+import com.appblade.framework.AppBlade;
+
 import android.content.ComponentName;
 import android.content.ServiceConnection;
 import android.os.IBinder;
+import android.util.Log;
 
 public class AppBladeSessionServiceConnection implements ServiceConnection {
 	public AppBladeSessionServiceConnection()
@@ -12,10 +15,13 @@ public class AppBladeSessionServiceConnection implements ServiceConnection {
 	
 	public void onServiceConnected(ComponentName name, IBinder service) {
 		// Do nothing. We only need the bind count at the moment.
+		Log.d(AppBlade.LogTag, "Service Connected");
 	}
 
 	public void onServiceDisconnected(ComponentName name) {
 		// Do nothing. We only need the bind count at the moment.
+		Log.d(AppBlade.LogTag, "Service Disconnected");
+
 	}
 
 
