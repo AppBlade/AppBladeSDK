52dfdc40256cad9b32193180f45b4dc6a4072bb1
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-03-14 10:01:01 -0400

    Lowering minSdkVersion to 7 Removing unused imports.

--- Android/AppBlade Framework/src/com/appblade/framework/authenticate/AuthHelper.java
+++ Android/AppBlade Framework/src/com/appblade/framework/authenticate/AuthHelper.java
@@ -1,24 +1,6 @@
 package com.appblade.framework.authenticate;
 
-import java.io.InputStream;
-import java.security.KeyStore;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-
-import org.apache.http.client.HttpClient;
-import org.apache.http.conn.ClientConnectionManager;
-import org.apache.http.conn.scheme.Scheme;
-import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.conn.ssl.SSLSocketFactory;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
-
-import com.appblade.framework.AppBlade;
 import com.appblade.framework.utils.StringUtils;
 
 import android.app.Activity;
@@ -26,7 +8,6 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnCancelListener;
 import android.content.Intent;
-import android.util.Log;
 
 /**
  * Helper class for functions related to authorization. These functions
--- Android/AppBlade Framework/src/com/appblade/framework/stats/SessionData.java
+++ Android/AppBlade Framework/src/com/appblade/framework/stats/SessionData.java
@@ -7,7 +7,6 @@ import java.util.Comparator;
 import java.util.Date;
 import java.util.Locale;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
