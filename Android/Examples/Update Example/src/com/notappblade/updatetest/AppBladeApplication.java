364133859a01daacc2590e4bf6d21f5a42086e45
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-05-09 11:56:55 -0400

    Updating the "Update Example" project to demonstrate the static library being added.

--- Android/Examples/Update Example/src/com/notappblade/updatetest/AppBladeApplication.java
+++ Android/Examples/Update Example/src/com/notappblade/updatetest/AppBladeApplication.java
@@ -7,10 +7,6 @@ public class AppBladeApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
-		String uuid = "56b5ec05-708e-4cb2-bd85-113ae9919852";
-		String token = "fa57d5800bda04a5f28b97d31d3ce4c4";
-		String secret = "6bfc0425a2414118d5c755eb19a96d73";
-		String issuance = "1363045440";
-		AppBlade.register(this, token, secret, uuid, issuance);
+		AppBlade.registerWithAssetFile(this.getApplicationContext());
 	}
 }
