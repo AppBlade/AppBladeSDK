dc10107ec96fb69f3f7dbc6cd128ff42b1d5aca4
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-05-13 18:40:17 -0400

    Adding all features to the Maps example.

--- iOS/Examples/Maps/Maps/MapsAppDelegate.m
+++ iOS/Examples/Maps/Maps/MapsAppDelegate.m
@@ -53,6 +53,9 @@
     /*
      Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
      */
+    AppBlade *blade = [AppBlade sharedManager];
+    [blade checkApproval];
+    [blade checkForUpdates];
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
