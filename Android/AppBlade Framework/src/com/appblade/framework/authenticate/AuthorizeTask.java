8f82e3a530539407b0d3061747ca3d39bbf65f78
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-06-24 09:55:50 -0400

    unused imports

--- Android/AppBlade Framework/src/com/appblade/framework/authenticate/AuthorizeTask.java
+++ Android/AppBlade Framework/src/com/appblade/framework/authenticate/AuthorizeTask.java
@@ -4,19 +4,13 @@ import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.apache.http.HttpRequest;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
-import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
