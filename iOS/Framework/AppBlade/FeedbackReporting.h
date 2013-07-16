07b893825bcd3b6df15d142761e69810cf9debed
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-07-16 17:04:05 -0400

    Giving FeedbackReporting some enum options.

--- iOS/Framework/AppBlade/FeedbackReporting.h
+++ iOS/Framework/AppBlade/FeedbackReporting.h
@@ -9,7 +9,23 @@
 #import <Foundation/Foundation.h>
 #import "FeedbackDialogue.h"
 
+#import "AppBladeWebClient.h"
 
-@interface FeedbackReporting : NSObject
+typedef NS_OPTIONS(NSUInteger, ABFeedbackSetupOptions) {
+    ABFeedbackSetupDefault                 = 0,      // default behavior
+    ABFeedbackSetupTripleFingerDoubleTap   = 1 <<  0,    // on all touch downs
+    ABFeedbackSetupCustomPrompt            = 1 <<  1    // on multiple touchdowns (tap count > 1)
+};
+
+typedef NS_OPTIONS(NSUInteger, ABFeedbackDisplayOptions) {
+    ABFeedbackDisplayDefault                 = 0,      // default behavior
+    ABFeedbackDisplayWithScreenshot          = 1 <<  0,   // Take a screenshot oto send with the feedback (default)
+    ABFeedbackDisplayWithoutScreenshot       = 1 <<  1    // Do not take a screenshot
+};
+
+
+@interface FeedbackReporting : AppBladeWebClient
+
+
 
 @end
