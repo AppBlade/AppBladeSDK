815e46662880f87b53d2911589645e1b894aac70
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-07-17 14:16:11 -0400

    Updating the KitchenSink to be less blank and bleak.

--- iOS/Examples/KitchenSink/KitchenSink/ViewController.h
+++ iOS/Examples/KitchenSink/KitchenSink/ViewController.h
@@ -9,5 +9,13 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+//form display button
+@property (strong, nonatomic) IBOutlet UIButton *showFormButton;
+- (IBAction)showFormButtonPressed:(id)sender;
+//crash buttons
+@property (strong, nonatomic) IBOutlet UIButton *crashButtonSigabrt;
+@property (strong, nonatomic) IBOutlet UIButton *crashButtonCustomException;
+@property (strong, nonatomic) IBOutlet UIButton *crashButtonSigsev;
+- (IBAction)crashButtonPressed:(id)sender;
 
 @end
--- iOS/Examples/KitchenSink/KitchenSink/ViewController.m
+++ iOS/Examples/KitchenSink/KitchenSink/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 
+#import "AppBlade.h"
+
 @interface ViewController ()
 
 @end
@@ -25,5 +27,74 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)showFormButtonPressed:(id)sender {
+    [[AppBlade sharedManager] showFeedbackDialogue];
+}
+
+- (IBAction)crashButtonPressed:(id)sender {
+    if(sender == self.crashButtonSigabrt){
+        [self sigabrt];
+    }else if(sender == self.crashButtonCustomException){
+        [self throwNSException];
+    }else if(sender == self.crashButtonSigsev){
+        [self sigsegv];
+    }else {
+        NSLog(@"Error causing error: Unknown sender.");
+    }
+}
+
+
+#pragma mark - crashes
+// credit to CrashKit for these .
+//https://github.com/kaler/CrashKit
+- (void)sigabrt
+{
+    abort();
+}
+
+- (void)sigbus
+{
+    void (*func)() = 0;
+    func();
+}
+
+- (void)sigfpe
+{
+    int zero = 0;  // LLVM is smart and actually catches divide by zero if it is constant
+    int i = 10/zero;
+    NSLog(@"Int: %i", i);
+}
+
+- (void)sigill
+{
+    typedef void(*FUNC)(void);
+    const static unsigned char insn[4] = { 0xff, 0xff, 0xff, 0xff };
+    void (*func)() = (FUNC)insn;
+    func();
+}
+
+- (void)sigpipe
+{
+    // Hmm, can't actually generate a SIGPIPE.
+    FILE *f = popen("ls", "r");
+    const char *buf[128];
+    pwrite(fileno(f), buf, 128, 0);
+}
+
+- (void)sigsegv
+{
+    // This actually raises a SIGBUS.
+    NSException *e = [NSException exceptionWithName:@"SIGSEGV" reason:@"Dummy SIGSEGV Reason" userInfo:nil];
+    @throw e;
+}
+
+- (void)throwNSException
+{
+    NSException *e = [NSException exceptionWithName:@"TestException" reason:@"Testing AppBlade Crash" userInfo:nil];
+    @throw e;
+}
+
+
 
 @end
