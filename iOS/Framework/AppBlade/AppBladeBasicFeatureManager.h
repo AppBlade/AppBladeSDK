6f0a8b2f6ac97952314e0e31a07f949a0a7b915a
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-07-17 12:47:15 -0400

    adding suggested pragmas to the basic manager.

--- iOS/Framework/AppBlade/AppBladeBasicFeatureManager.h
+++ iOS/Framework/AppBlade/AppBladeBasicFeatureManager.h
@@ -8,11 +8,19 @@
 
 #import <Foundation/Foundation.h>
 #import "AppBladeWebOperation.h"
+#import "AppBladeLogging.h"
+#import "AppBladeSharedConstants.h"
 
 @protocol AppBladeBasicFeatureManager
 
 @required
     - (id)initWithDelegate:(id<AppBladeWebOperationDelegate>)delegate;
     - (AppBladeWebOperation*) generateWebRequest;
-
+//Suggested pragma structure (after implementing the required methods, which should always be first)
+#pragma mark - Web Request Generators
+//wherein you generate the unique web request for the SDK
+#pragma mark Stored Web Request Handling
+//wherein you implement any storage behavior for pending API calls.
+//...
+//then whatever else you feel like
 @end
