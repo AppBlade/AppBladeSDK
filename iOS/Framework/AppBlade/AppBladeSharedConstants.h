af06a3090872eaf2f69f01883ef88f405730db66
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-07-22 17:52:44 -0400

    Moving Feedback manager enums to the shared contents header so they can be more accessible.

--- iOS/Framework/AppBlade/AppBladeSharedConstants.h
+++ iOS/Framework/AppBlade/AppBladeSharedConstants.h
@@ -76,4 +76,19 @@ static NSString* const kAppBladeApiTokenResponseDeviceSecretKey     = @"device_s
 static NSString* const kAppBladeApiTokenResponseTimeToLiveKey       = @"ttl";
 
 
+//Feature enums
+typedef NS_OPTIONS(NSUInteger, AppBladeFeedbackSetupOptions) {
+    AppBladeFeedbackSetupDefault                 = 0,      // default behavior
+    AppBladeFeedbackSetupTripleFingerDoubleTap   = 1 <<  0,    // on all touch downs
+    AppBladeFeedbackSetupCustomPrompt            = 1 <<  1    // on multiple touchdowns (tap count > 1)
+};
+
+typedef NS_OPTIONS(NSUInteger, AppBladeFeedbackDisplayOptions) {
+    AppBladeFeedbackDisplayDefault                 = 0,      // default behavior
+    AppBladeFeedbackDisplayWithScreenshot          = 1 <<  0,   // Take a screenshot oto send with the feedback (default)
+    AppBladeFeedbackDisplayWithoutScreenshot       = 1 <<  1    // Do not take a screenshot
+};
+
+
+
 #endif
--- iOS/Framework/AppBlade/FeedbackReportingManager.h
+++ iOS/Framework/AppBlade/FeedbackReportingManager.h
@@ -12,18 +12,6 @@
 #import "AppBladeWebOperation.h"
 #import "AppBladeBasicFeatureManager.h"
 
-typedef NS_OPTIONS(NSUInteger, AppBladeFeedbackSetupOptions) {
-    AppBladeFeedbackSetupDefault                 = 0,      // default behavior
-    AppBladeFeedbackSetupTripleFingerDoubleTap   = 1 <<  0,    // on all touch downs
-    AppBladeFeedbackSetupCustomPrompt            = 1 <<  1    // on multiple touchdowns (tap count > 1)
-};
-
-typedef NS_OPTIONS(NSUInteger, AppBladeFeedbackDisplayOptions) {
-    AppBladeFeedbackDisplayDefault                 = 0,      // default behavior
-    AppBladeFeedbackDisplayWithScreenshot          = 1 <<  0,   // Take a screenshot oto send with the feedback (default)
-    AppBladeFeedbackDisplayWithoutScreenshot       = 1 <<  1    // Do not take a screenshot
-};
-
 
 @interface FeedbackReportingManager : NSObject<AppBladeBasicFeatureManager>
     @property (nonatomic, strong) id<AppBladeWebOperationDelegate> delegate;
