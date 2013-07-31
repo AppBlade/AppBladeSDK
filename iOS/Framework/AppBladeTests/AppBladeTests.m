b6b1d0c6493dc12411adeb259829888c692cf6ac
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-07-31 09:12:19 -0400

    renaming test methods to be more manually ordered.

--- iOS/Framework/AppBladeTests/AppBladeTests.m
+++ iOS/Framework/AppBladeTests/AppBladeTests.m
@@ -10,26 +10,43 @@
 #import <UIKit/UIKit.h>
 #import "AppBlade.h"
 
+#define kAppBladeTestPlistName @"TestAppBladeKeys"
+#define kAppBladeTestNonExistentPlistName @"TestAppBladeKeysDoesNotExist"
+
+
 @implementation AppBladeTests
 
 - (void)setUp
 {
     [super setUp];
-    
     // Set-up code here.
 }
 
 - (void)tearDown
 {
     // Tear-down code here.
-    
     [super tearDown];
 }
 
-- (void)testSdkVersionExists
+- (void)test01SdkVersionExists
 {
     NSString* sdkVersion = [AppBlade sdkVersion];
     STAssertNotNil(sdkVersion, @"Could not find SDK version.");
 }
+
+
+- (void)test02CacheDirectoryCreates
+{
+    [[AppBlade sharedManager] checkAndCreateAppBladeCacheDirectory];
+    BOOL isDirectory = YES;
+    STAssertTrue([[NSFileManager defaultManager] fileExistsAtPath:[AppBlade cachesDirectoryPath] isDirectory:&isDirectory], @"Could not create cache directory.");
+    STAssertTrue(isDirectory, @"Cache directory exists but is not a directory.");
+}
+
+- (void)test03NonExistentPlistDisablesSDK
+{
+    
+}
+
 
 @end
