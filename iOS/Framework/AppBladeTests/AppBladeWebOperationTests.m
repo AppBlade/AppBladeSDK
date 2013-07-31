6ae619f1b27f12a10ed30c7e0cba6d981155b623
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-07-31 09:12:49 -0400

    adding the beginning of WebOperation tests

--- iOS/Framework/AppBladeTests/AppBladeWebOperationTests.m
+++ iOS/Framework/AppBladeTests/AppBladeWebOperationTests.m
@@ -7,7 +7,55 @@
 //
 
 #import "AppBladeWebOperationTests.h"
+#import "AppBladeWebOperation.h"
+#import "AppBlade.h"
+
+
+#define kAppBladeTestHostURL @"https://appblade.com/"
+#define kAppBladeTestPlistName @"TestAppBladeKeys"
+
 
 @implementation AppBladeWebOperationTests
+
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+-(void) test01ConnectToAppBlade
+{
+    NSURL *url = [NSURL URLWithString:kAppBladeTestHostURL];
+    NSData *data = [NSData dataWithContentsOfURL:url];
+    STAssertNotNil(data, @"Connection to %@ could not be made", kAppBladeTestHostURL);
+}
+
+-(void) test02ConnectWithProjectSecret
+{
+    [[AppBlade sharedManager] registerWithAppBladePlistNamed:kAppBladeTestPlistName];
+}
+
+-(void) test03
+{
+    
+}
+
+// What happens when we make many calls at once? They should queue up, one at a time. This will
+// test this feature, and override the callbacks specified in the plist so we can count the callbacks.
+-(void) test04MultipleRequests
+{
+
+}
+
+#pragma mark - Verification Methods
+
+#pragma mark - Validation Callbacks
+
 
 @end
