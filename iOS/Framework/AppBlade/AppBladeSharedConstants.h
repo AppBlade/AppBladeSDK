b35caa91d9a8f2d3b0c19433dbf1cdd513fbeede
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-08-30 16:28:00 -0400

    typo in the comments

--- iOS/Framework/AppBlade/AppBladeSharedConstants.h
+++ iOS/Framework/AppBlade/AppBladeSharedConstants.h
@@ -80,13 +80,13 @@ static NSString* const kAppBladeApiTokenResponseTimeToLiveKey       = @"ttl";
 //Feature enums
 typedef NS_OPTIONS(NSUInteger, AppBladeFeedbackSetupOptions) {
     AppBladeFeedbackSetupDefault                 = 0,      // default behavior
-    AppBladeFeedbackSetupTripleFingerDoubleTap   = 1 <<  0,    // on all touch downs
+    AppBladeFeedbackSetupTripleFingerDoubleTap   = 1 <<  0,   // on all touch downs
     AppBladeFeedbackSetupCustomPrompt            = 1 <<  1    // on multiple touchdowns (tap count > 1)
 };
 
 typedef NS_OPTIONS(NSUInteger, AppBladeFeedbackDisplayOptions) {
     AppBladeFeedbackDisplayDefault                 = 0,      // default behavior
-    AppBladeFeedbackDisplayWithScreenshot          = 1 <<  0,   // Take a screenshot oto send with the feedback (default)
+    AppBladeFeedbackDisplayWithScreenshot          = 1 <<  0,   // Take a screenshot to send with the feedback (default)
     AppBladeFeedbackDisplayWithoutScreenshot       = 1 <<  1    // Do not take a screenshot
 };
 
