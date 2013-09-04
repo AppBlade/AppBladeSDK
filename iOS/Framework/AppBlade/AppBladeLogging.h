8a6ed8c1250bd67ff148a3ac00411846b251d9d7
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-09-04 14:50:48 -0400

    reformatting our macros for better readability

--- iOS/Framework/AppBlade/AppBladeLogging.h
+++ iOS/Framework/AppBlade/AppBladeLogging.h
@@ -14,14 +14,22 @@
 
 #define AppBlade_AppBladeLogging_h
     #ifdef APPBLADE_DEBUG_LOGGING
-        #define ABDebugLog_internal( s, ... ) NSLog( @"<%@:(%d)> %@", [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__, [NSString stringWithFormat:(s), ##__VA_ARGS__] )
+        #define ABDebugLog_internal( s, ... )\
+        ({\
+            NSLog( @"<%@:(%d)> %@", [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__, [NSString stringWithFormat:(s), ##__VA_ARGS__] ); \
+        })
     #else
-        #define ABDebugLog_internal( s, ... ) //Do nothing
+        #define ABDebugLog_internal( s, ... )\
+        ({ }) //Do nothing
     #endif
 
     #ifdef APPBLADE_ERROR_LOGGING
-        #define ABErrorLog( s, ... ) NSLog( @"< %@:(%d)> %@", [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__, [NSString stringWithFormat:(s), ##__VA_ARGS__] )
+        #define ABErrorLog( s, ... )\
+        ({\
+            NSLog( @"<%@:(%d)> %@", [[NSString stringWithUTF8String:__FILE__] lastPathComponent], __LINE__, [NSString stringWithFormat:(s), ##__VA_ARGS__] ); \
+        })
     #else
-        #define ABErrorLog( s, ... ) //Do nothing
+        #define ABErrorLog( s, ... )\
+        ({ }) //Do nothing
     #endif
 #endif
