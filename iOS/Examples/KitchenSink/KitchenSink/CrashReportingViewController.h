2c379718874a0543db1dd164d224d794c31823c2
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-09-04 07:29:14 -0400

    Adding secondary view controllers for certain features.
    Crash Reporting and Custom Parameters

---
+++ iOS/Examples/KitchenSink/KitchenSink/CrashReportingViewController.h
@@ -0,0 +1,13 @@
+//
+//  CrashReportingViewController.h
+//  KitchenSink
+//
+//  Created by AndrewTremblay on 9/4/13.
+//  Copyright (c) 2013 Raizlabs Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CrashReportingViewController : UIViewController
+
+@end
