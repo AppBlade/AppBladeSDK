00ab461d07dff3fde530823de8f0a14cdef7949a
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-09-04 10:02:25 -0400

    fixing edge insets

--- iOS/Examples/KitchenSink/KitchenSink/ViewController.m
+++ iOS/Examples/KitchenSink/KitchenSink/ViewController.m
@@ -19,6 +19,23 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    //Set Button Image insets
+    UIEdgeInsets insetsExample = UIEdgeInsetsMake(12, 12, 12, 12);
+    [self setBackgroundImageInsets:insetsExample forButton:self.showFormButton];
+    [self setBackgroundImageInsets:insetsExample forButton:self.crashButtonSigabrt];
+    [self setBackgroundImageInsets:insetsExample forButton:self.crashButtonCustomException];
+    [self setBackgroundImageInsets:insetsExample forButton:self.crashButtonSigsev];
+    [self setBackgroundImageInsets:insetsExample forButton:self.crashOptionsListButton];
+    [self setBackgroundImageInsets:insetsExample forButton:self.startSessionButton];
+    [self setBackgroundImageInsets:insetsExample forButton:self.endSessionButton];
+    [self setBackgroundImageInsets:insetsExample forButton:self.seeCurrentParamsButton];
+    [self setBackgroundImageInsets:insetsExample forButton:self.setNewParameterButton];
+    [self setBackgroundImageInsets:insetsExample forButton:self.clearAllParamsButton];
+    [self setBackgroundImageInsets:insetsExample forButton:self.checkUpdatesButton];
+    [self setBackgroundImageInsets:insetsExample forButton:self.authenticationButton];
+    
+    
 	//Build the cards in the scroll view
     CGFloat totalHeight = 0.0f;
     totalHeight = [self addView:self.headerWrapperView toScrollView:self.scrollView atVertOffset:totalHeight];
@@ -28,8 +45,7 @@
     totalHeight = [self addView:self.customParamsWrapperView toScrollView:self.scrollView atVertOffset:totalHeight];
     
     totalHeight = [self addView:self.updateCheckingWrapperView toScrollView:self.scrollView atVertOffset:totalHeight];
-    totalHeight = [self addView:self.authenticationWrapperView toScrollView:self.scrollView atVertOffset:totalHeight];
-    
+    totalHeight = [self addView:self.authenticationWrapperView toScrollView:self.scrollView atVertOffset:totalHeight];    
     [self.scrollView setContentSize:CGSizeMake(self.view.bounds.size.width, totalHeight)];
 }
 
@@ -39,6 +55,14 @@
     viewFrame.origin.y = height;
     [view setFrame:viewFrame];
     return height + viewFrame.size.height;
+}
+
+-(void)setBackgroundImageInsets:(UIEdgeInsets)insets forButton:(UIButton*)button
+{
+    UIImage *bgImageNormal = [UIImage imageNamed:@"card-btn-normal@2x.png"];
+    UIImage *bgImagePressed = [UIImage imageNamed:@"card-btn-pressed@2x.png"];
+    [button setBackgroundImage:[bgImageNormal resizableImageWithCapInsets:insets resizingMode:UIImageResizingModeStretch] forState:UIControlStateNormal];
+    [button setBackgroundImage:[bgImagePressed resizableImageWithCapInsets:insets resizingMode:UIImageResizingModeStretch] forState:UIControlStateHighlighted];
 }
 
 - (void)didReceiveMemoryWarning
