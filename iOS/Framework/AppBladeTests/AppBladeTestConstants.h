7b886c16cae42321516c383723dae240fe6c4d26
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-09-10 16:49:30 -0400

    Adding a header for shared Test Constants that we will likely be using.

---
+++ iOS/Framework/AppBladeTests/AppBladeTestConstants.h
@@ -0,0 +1,17 @@
+//
+//  AppBladeTestConstants.h
+//  AppBlade
+//
+//  Created by AndrewTremblay on 9/10/13.
+//  Copyright (c) 2013 Raizlabs. All rights reserved.
+//
+
+#ifndef AppBlade_AppBladeTestConstants_h
+#define AppBlade_AppBladeTestConstants_h
+
+#define kAppBladeTestPlistName @"TestAppBladeKeys"
+#define kAppBladeTestNonExistentPlistName @"TestAppBladeKeysDoesNotExist"
+#define kNetworkPatience 5.0
+
+
+#endif
