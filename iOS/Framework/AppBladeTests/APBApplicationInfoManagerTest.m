b8ac4f1de531eaad5adde7b9ae1a959207969221
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-09-10 18:05:36 -0400

    Adding test cases for ApplicationInfoManager
    including APPBLADE_TEST_FAIRPLAY_ENCRYPTED preprocessor.

--- iOS/Framework/AppBladeTests/APBApplicationInfoManagerTest.m
+++ iOS/Framework/AppBladeTests/APBApplicationInfoManagerTest.m
@@ -14,4 +14,48 @@
 @implementation APBApplicationInfoManagerTest
 
 
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    [super tearDown];
+}
+
+- (void)test01executableUUIDExists
+{
+    NSString* executableUUID = [[AppBlade sharedManager] executableUUID];
+    STAssertNotNil(executableUUID, @"Could not retrieve an executableUUID.");
+}
+
+
+- (void)test02hashInfoPlistExists
+{
+    NSString* hashInfoPlist = [[AppBlade sharedManager] hashInfoPlist];
+    STAssertNotNil(hashInfoPlist, @"Could not retrieve the hash info plist.");
+}
+
+- (void)test03hashExecutableExists
+{
+    NSString* hashExecutable = [[AppBlade sharedManager] hashExecutable];
+    STAssertNotNil(hashExecutable, @"Could not retrieve an executable hash.");
+}
+
+
+- (void)test04isAppStoreBuildReturns
+{
+    BOOL executableUUID = [[AppBlade sharedManager] isAppStoreBuild];
+#ifdef APPBLADE_TEST_FAIRPLAY_ENCRYPTED
+    STAssertTrue(executableUUID, @"Could not assert appstore build was true.");
+#else
+    STAssertFalse(executableUUID, @"Could not invalid appstore build detected.");
+#endif
+}
+
+
+
+
 @end
