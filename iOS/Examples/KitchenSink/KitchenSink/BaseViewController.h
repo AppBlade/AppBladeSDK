4b2ecddf85b33cd634e684b5e766cb4c81e7cf9d
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-09-11 15:56:32 -0400

    Adding a BaseViewController to simplify things.

---
+++ iOS/Examples/KitchenSink/KitchenSink/BaseViewController.h
@@ -0,0 +1,16 @@
+//
+//  BaseViewController.h
+//  KitchenSink
+//
+//  Created by AndrewTremblay on 9/11/13.
+//  Copyright (c) 2013 Raizlabs Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+-(void)setBackgroundImageInsets:(UIEdgeInsets)insets forButton:(UIButton*)button;
+-(CGFloat)addView:(UIView *)view toScrollView:(UIScrollView *)scrollView atVertOffset:(CGFloat)height;
+
+@end
---
+++ iOS/Examples/KitchenSink/KitchenSink/BaseViewController.m
@@ -0,0 +1,56 @@
+//
+//  BaseViewController.m
+//  KitchenSink
+//
+//  Created by AndrewTremblay on 9/11/13.
+//  Copyright (c) 2013 Raizlabs Corporation. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface BaseViewController ()
+
+@end
+
+@implementation BaseViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+-(void)setBackgroundImageInsets:(UIEdgeInsets)insets forButton:(UIButton*)button
+{
+    UIImage *bgImageNormal = [UIImage imageNamed:@"card-btn-normal@2x.png"];
+    UIImage *bgImagePressed = [UIImage imageNamed:@"card-btn-pressed@2x.png"];
+    [button setBackgroundImage:[bgImageNormal resizableImageWithCapInsets:insets resizingMode:UIImageResizingModeStretch] forState:UIControlStateNormal];
+    [button setBackgroundImage:[bgImagePressed resizableImageWithCapInsets:insets resizingMode:UIImageResizingModeStretch] forState:UIControlStateHighlighted];
+}
+
+-(CGFloat)addView:(UIView *)view toScrollView:(UIScrollView *)scrollView atVertOffset:(CGFloat)height
+{
+    [scrollView addSubview:view];
+    CGRect viewFrame = view.frame;
+    viewFrame.origin.y = height;
+    [view setFrame:viewFrame];
+    return height + viewFrame.size.height;
+}
+
+@end
