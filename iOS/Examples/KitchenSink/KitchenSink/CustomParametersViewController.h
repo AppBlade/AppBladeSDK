3cc81ca1265002b18623be6146661d3411efd76a
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-09-11 14:36:50 -0400

    Expanding customParamsViewController xib and objects.

--- iOS/Examples/KitchenSink/KitchenSink/CustomParametersViewController.h
+++ iOS/Examples/KitchenSink/KitchenSink/CustomParametersViewController.h
@@ -9,5 +9,30 @@
 #import <UIKit/UIKit.h>
 
 @interface CustomParametersViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIScrollView *customParamsScrollView;
+
+
+@property (strong, nonatomic) IBOutlet UIView *headerView;
+
+
+@property (strong, nonatomic) IBOutlet UIView *addParamView;
+@property (strong, nonatomic) IBOutlet UITextField *keyTextField;
+@property (strong, nonatomic) IBOutlet UITextField *valueTextField;
+- (IBAction)submitNewCustomParam:(id)sender;
+
+
+@property (strong, nonatomic) IBOutlet UIButton *keyInfoButton;
+@property (strong, nonatomic) IBOutlet UIButton *valueInfoButton;
+- (IBAction)infoButtonPressed:(id)sender;
+
+
+@property (strong, nonatomic) IBOutlet UIView *currentParamView;
+@property (strong, nonatomic) IBOutlet UITextView *currentParamsTextView;
+
+
+@property (strong, nonatomic) IBOutlet UIView *actionsView;
+@property (strong, nonatomic) IBOutlet UIButton *showFeedbackDialogButton;
+@property (strong, nonatomic) IBOutlet UIButton *clearParamsButton;
+- (IBAction)actionButtonPressed:(id)sender;
 
 @end
--- iOS/Examples/KitchenSink/KitchenSink/CustomParametersViewController.m
+++ iOS/Examples/KitchenSink/KitchenSink/CustomParametersViewController.m
@@ -35,4 +35,44 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)viewWillLayoutSubviews
+{
+    //Set Button Image insets
+//    UIEdgeInsets insetsExample = UIEdgeInsetsMake(12, 12, 12, 12);
+//    [self setBackgroundImageInsets:insetsExample forButton:self.showFormButton];
+    
+	//Build the cards in the scroll view
+    CGFloat totalHeight = 0.0f;
+    totalHeight = [self addView:self.headerView toScrollView:self.customParamsScrollView atVertOffset:totalHeight];
+    totalHeight = [self addView:self.addParamView toScrollView:self.customParamsScrollView atVertOffset:totalHeight];
+    totalHeight = [self addView:self.currentParamView toScrollView:self.customParamsScrollView atVertOffset:totalHeight];
+    totalHeight = [self addView:self.actionsView toScrollView:self.customParamsScrollView atVertOffset:totalHeight];
+    [self.customParamsScrollView setContentSize:CGSizeMake(self.view.bounds.size.width, totalHeight)];
+}
+
+-(CGFloat)addView:(UIView *)view toScrollView:(UIScrollView *)scrollView atVertOffset:(CGFloat)height {
+    [scrollView addSubview:view];
+    CGRect viewFrame = view.frame;
+    viewFrame.origin.y = height;
+    [view setFrame:viewFrame];
+    return height + viewFrame.size.height;
+}
+
+-(void)setBackgroundImageInsets:(UIEdgeInsets)insets forButton:(UIButton*)button
+{
+    UIImage *bgImageNormal = [UIImage imageNamed:@"card-btn-normal@2x.png"];
+    UIImage *bgImagePressed = [UIImage imageNamed:@"card-btn-pressed@2x.png"];
+    [button setBackgroundImage:[bgImageNormal resizableImageWithCapInsets:insets resizingMode:UIImageResizingModeStretch] forState:UIControlStateNormal];
+    [button setBackgroundImage:[bgImagePressed resizableImageWithCapInsets:insets resizingMode:UIImageResizingModeStretch] forState:UIControlStateHighlighted];
+}
+
+- (IBAction)submitNewCustomParam:(id)sender {
+}
+
+- (IBAction)actionButtonPressed:(id)sender {
+}
+
+
+- (IBAction)infoButtonPressed:(id)sender {
+}
 @end
