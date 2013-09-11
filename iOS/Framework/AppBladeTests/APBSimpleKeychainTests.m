45f4652366f90f29f87655d64d8464eff85cc6e6
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-09-11 09:00:58 -0400

    adding struts for simple keychain tests

--- iOS/Framework/AppBladeTests/APBSimpleKeychainTests.m
+++ iOS/Framework/AppBladeTests/APBSimpleKeychainTests.m
@@ -8,6 +8,42 @@
 
 #import "APBSimpleKeychainTests.h"
 
+#import "APBSimpleKeychain.h"
+
 @implementation APBSimpleKeychainTests
+- (void)setUp
+{
+    [super setUp];
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    [APBSimpleKeychain deleteLocalKeychain]; //Deletes every deletable thing we have in the app.
+    [super tearDown];
+}
+
+
+-(void)test01keychainStoresAndLoads
+{
+
+}
+
+-(void)test02keychainDeletesSpecificKey
+{
+    
+}
+
+-(void)test03errorMessageReturnsValidValue
+{
+    
+}
+
+-(void)test04sanitizeClearsKeychain
+{
+    
+}
+
+
 
 @end
