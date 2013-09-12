c57409b7f6848b9851a495f6c650ccffaee9f06a
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-09-12 15:11:06 -0400

    DeviceFeatureViewController created

---
+++ iOS/Examples/KitchenSink/KitchenSink/DeviceFeatureViewController.h
@@ -0,0 +1,13 @@
+//
+//  DeviceFeatureViewController.h
+//  KitchenSink
+//
+//  Created by AndrewTremblay on 9/12/13.
+//  Copyright (c) 2013 Raizlabs Corporation. All rights reserved.
+//
+
+#import "BaseFeatureViewController.h"
+
+@interface DeviceFeatureViewController : BaseFeatureViewController
+
+@end
---
+++ iOS/Examples/KitchenSink/KitchenSink/DeviceFeatureViewController.m
@@ -0,0 +1,38 @@
+//
+//  DeviceFeatureViewController.m
+//  KitchenSink
+//
+//  Created by AndrewTremblay on 9/12/13.
+//  Copyright (c) 2013 Raizlabs Corporation. All rights reserved.
+//
+
+#import "DeviceFeatureViewController.h"
+
+@interface DeviceFeatureViewController ()
+
+@end
+
+@implementation DeviceFeatureViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
