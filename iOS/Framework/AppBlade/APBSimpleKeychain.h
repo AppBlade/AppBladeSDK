8e5e9589365d9f4c9cf1c9ec55ecc99430b2d01b
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-09-13 10:17:45 -0400

    doc/commenting the keychain

--- iOS/Framework/AppBlade/APBSimpleKeychain.h
+++ iOS/Framework/AppBlade/APBSimpleKeychain.h
@@ -14,17 +14,63 @@
 @class SimpleKeychainUserPass;
 
 @interface APBSimpleKeychain : NSObject
+/*!
+ @abstract Returns true when the app has acceptable keychain access.
+ @discussion Checks basic keychain permissions of reading and writing and deleting. Our internal requirements may change accross devices,  processors, app signage, and function inclusion.
+ @return true If accessible keychain access was met.
+ */
 + (BOOL)hasKeychainAccess;
 
+/*!
+ @abstract Removes the entry for the given service from keychain.
+ @discussion Accepts service name and NSCoding-complaint data object. Automatically overwrites if something exists.
+ @return Returns true if no errors occcurred on deletion
+ */
 + (BOOL)delete:(NSString *)service;
+
+/*!
+ @abstract Accepts service name and NSCoding-complaint data object and storeds it to the local keychain.
+ @discussion Accepts service name and NSCoding-complaint data object. Automatically overwrites if something exists.
+ @return Returns true if no errors occcurred on save
+ */
 + (BOOL)save:(NSString *)service data:(id)data;
+
+/*!
+ @abstract Loads the stored keychain data from the entered service
+ @discussion Casting the object is left to the developer, only NSCoding-complaint data objects are accepted to be stored, so a simple cast to <pre>NSString</pre> or <pre>NSDictionary</pre> should be all that's required.
+ @return An object inflated from the data stored in the keychain entry for the given service.
+ */
 + (id)load:(NSString *)service;
 
+/*!
+ @abstract Provides a human-readable error message from the OSErrorCode
+ @discussion This error message is provided in the MAC SDK, but not iOS. The reason for that is unknown.
+ @param errorCode The error code you wish to translate into english. 
+ @return A human readable error string
+ */
 +(NSString*) errorMessageFromCode:(OSStatus)errorCode;
 
-+ (void)deleteLocalKeychain; //Deletes every deletable thing we have in the app.
-+ (BOOL)keychainInconsistencyExists; //our current check for keychain inconsistency
+/*!
+ @abstract Deletes every deletable thing we have in the app via a clever for loop.
+ @discussion This is obviously a very dangerous function which should never be used in production. It's useful for development and testing devices that retrieve differently signed builds. Use it as you would a stick of dynamite; clear out a construction site with it, don't use it to tidy up the living room. 
+ @return Ω
+*/
++ (void)deleteLocalKeychain;
 
-+ (void)sanitizeKeychain; //a helper function for that's essentially deleteLocalKeychain if keychainInconsistencyExists
+
+/*!
+ @abstract Checks for any obvious inconsitencies in the keychain
+ @discussion This function checks basic functions of the keychain, e.g. whether a written value can be immediately retrieved, if it can be deleted, etcetera. It does not detect keychin permissions directly. That is for +(BOOL)hasKeychainAccess to do.
+ This by no means is guaranteed to catch all keychain problems, but it's our function and it seems to catch the ones we need to catch.
+ 
+ @return true if our internal checks detect something terribly wrong with the keychain
+ */
++ (BOOL)keychainInconsistencyExists;
+
+/*!
+ @abstract Attempts to conditionally wipe the entire keychain. 
+ @discussion It's essentially deleteLocalKeychain if keychainInconsistencyExists. Even with the catch in place, this function is also not reccommended for production.
+ */
++ (void)sanitizeKeychain; 
 
 @end
