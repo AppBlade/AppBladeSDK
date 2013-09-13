6aab3c179f9a1f3b105d8450128f23335b911728
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-09-13 10:28:11 -0400

    Adding tests for authentication manager.

---
+++ iOS/Framework/AppBladeTests/APBAuthenticationManagerTests.h
@@ -0,0 +1,13 @@
+//
+//  APBAuthenticationManagerTests.h
+//  AppBlade
+//
+//  Created by AndrewTremblay on 9/13/13.
+//  Copyright (c) 2013 Raizlabs. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface APBAuthenticationManagerTests : SenTestCase
+
+@end
---
+++ iOS/Framework/AppBladeTests/APBAuthenticationManagerTests.m
@@ -0,0 +1,13 @@
+//
+//  APBAuthenticationManagerTests.m
+//  AppBlade
+//
+//  Created by AndrewTremblay on 9/13/13.
+//  Copyright (c) 2013 Raizlabs. All rights reserved.
+//
+
+#import "APBAuthenticationManagerTests.h"
+
+@implementation APBAuthenticationManagerTests
+
+@end
