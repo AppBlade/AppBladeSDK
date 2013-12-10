e99a8b8e31c932a17378948f43a7166a0171b1e0
Andrew Tremblay <gloriousrocketship@gmail.com>
2013-12-09 21:24:46 -0800

    making createTablesWithDelegate optional in the prototype

--- iOS/Framework/AppBlade/APBBasicFeatureManager.h
+++ iOS/Framework/AppBlade/APBBasicFeatureManager.h
@@ -16,17 +16,22 @@
 @protocol APBBasicFeatureManager
 
 @required
+//every manager delegate has to include the APBDataManagerDelegate regardless of database use
 - (id)initWithDelegate:(id<APBWebOperationDelegate, APBDataManagerDelegate>)webOpAndDataManagerDelegate;
+
+@optional
 
 //Suggested pragma structure (after implementing the required methods, which should always be first)
 #pragma mark - Web Request Generators
 //wherein you generate the unique web request for the SDK, please use the Blocks whenever possible
 #pragma mark Stored Web Request Handling
--(void)createTablesWithDelegate:(id<APBDataManagerDelegate>)databaseDelegate;
 //wherein you implement any storage behavior for pending API calls.
 
-//then whatever else you feel like
-@optional
+-(void)createTablesWithDelegate:(id<APBDataManagerDelegate>)databaseDelegate;
+
 -(NSString *)getDefaultForeignKeyDefinition:(NSString *)referencingColumn;
+
+//then whatever else you feel like
+
 
 @end
