1bcc8253b72d5648a899c42997ae22d521884050
Andrew Tremblay <gloriousrocketship@gmail.com>
2014-01-12 18:57:19 -0500

    Adding a custom parameters compatibility layer for database objects that use the removable custom parameters data object.

---
+++ iOS/Framework/AppBlade/AppBladeDatabaseObject+ CustomParametersCompatibility.m
@@ -0,0 +1,52 @@
+//
+//  AppBladeDatabaseObject+CustomParamtersCompatibilityLayer.m
+//  AppBlade
+//
+//  Created by AndrewTremblay on 1/11/14.
+//  Copyright (c) 2014 AppBlade Corporation. All rights reserved.
+//
+
+#import "AppBladeDatabaseObject+CustomParametersCompatibility.h"
+
+#ifndef SKIP_CUSTOM_PARAMS
+#import "AppBlade.h"
+#import "APBCustomParametersManager.h"
+#endif
+
+@implementation AppBladeDatabaseObject (CustomParametersCompatibility)
+@dynamic customParameterId;
+
+-(NSDictionary *)getCustomParamSnapshot {
+#ifndef SKIP_CUSTOM_PARAMS
+    return [self.customParameterObj asDictionary];
+#else
+    return @{ };
+#endif
+}
+
+-(void)setCustomParamSnapshot {
+#ifndef SKIP_CUSTOM_PARAMS
+    if(self.customParameterId == nil){
+        NSError *error = nil;
+        APBDatabaseCustomParameter *newCustomParamDataObj = [[[AppBlade sharedManager] customParamsManager]  generateCustomParameterFromCurrentParamsWithError:&error];
+        self.customParameterId = [newCustomParamDataObj getId];
+    }//currently we only cover setting the custom parameter once.
+#endif
+    //if we don't have custom parameters enabled, this call does nothing
+}
+
+#ifndef SKIP_CUSTOM_PARAMS
+-(APBDatabaseCustomParameter *)customParameterObj{
+    //lookup custom parameter obj, this should occur rarely if ever.
+    return [[[AppBlade sharedManager] customParamsManager] getCustomParamById:self.customParameterId];
+}
+
+
+
+
+
+
+
+#endif
+
+@end
---
+++ iOS/Framework/AppBlade/AppBladeDatabaseObject+CustomParametersCompatibility.h
@@ -0,0 +1,27 @@
+//
+//  AppBladeDatabaseObject+CustomParamtersCompatibility.h
+//  AppBlade
+//
+//  Created by AndrewTremblay on 1/11/14.
+//  Copyright (c) 2014 AppBlade Corporation. All rights reserved.
+//
+
+#import "AppBladeDatabaseObject.h"
+#import "APBDatabaseCustomParameter.h"
+
+//database object that database object classes will always need to import in order to have / not have custom parameters attached to them
+/* steps to use:
+    import this header file.
+ @property (nonatomic, strong) NSString *customParameterId; //must be implemented in the class
+ 
+ 
+ */
+
+@interface AppBladeDatabaseObject (CustomParametersCompatibility)
+    -(NSDictionary *)getCustomParamSnapshot; //Returns custom params when the feature is enabled. False otherwise.
+    -(void)setCustomParamSnapshot;  //sets custom param object from the current parameters.
+    @property (nonatomic, strong) NSString *customParameterId; //must be implemented in the class
+
+-(APBDatabaseCustomParameter *)customParameterObj;
+
+@end
